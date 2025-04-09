b43d955fee0c12851ee8cef9225397d5ee7410e3
Adriellison Ferreira <adriellisonki@gmail.com>
2025-04-09 14:22:46 -0300

    feat: Criado UserSeeder para popular a tabela de Usuários;

---
+++ API-CRUD-LARAVEL-11/database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+        ]);
+    }
+}
