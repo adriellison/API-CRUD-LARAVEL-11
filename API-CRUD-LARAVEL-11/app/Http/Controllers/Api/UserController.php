bf748df2153c66c34eef74ff7276a2c067d332ab
Adriellison Ferreira <adriellisonki@gmail.com>
2025-04-09 17:43:32 -0300

    feat: Adicionado páginação no retorno dos usuários do endpoint GetUsers; feat: Adicionado retorno de usuários por Id no endpoint GetUserById; feat: Criado testes de endpoint para o GetUsers e GetUsersById no client.http

--- API-CRUD-LARAVEL-11/app/Http/Controllers/Api/UserController.php
+++ API-CRUD-LARAVEL-11/app/Http/Controllers/Api/UserController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index() : JsonResponse
     {
-        $users = User::orderBy('id', 'desc')->get();
+        $users = User::orderBy('id', 'desc')->paginate(5);
         
         return response()->json([
             'status' => true,
             'users' => $users
+        ], 200);
+    }
+
+    public function show(User $user) : JsonResponse
+    {
+        return response()->json([
+            'status' => true,
+            'user' => $user
         ], 200);
     }
 }
--- API-CRUD-LARAVEL-11/routes/api.php
+++ API-CRUD-LARAVEL-11/routes/api.php
@@ -11,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/users/{user}', [UserController::class, 'show']);
